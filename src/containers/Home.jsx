4d665c707bc235b58bfaa5f04f6d4546a9501c02
Angelo Leva <angelo.leva@tranzact.net>
2020-10-15 23:30:27 -0500

    added dates

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -5,6 +5,7 @@ import Services from "../components/Services";
 import Service from "../components/Service";
 import Call from "../components/Call";
 import { db } from "../services/firebase";
+import firebase from "firebase";
 
 const Home = () => {
   const [appointments, setAppointments] = useState([]);
@@ -28,13 +29,36 @@ const Home = () => {
       .get()
       .then((querySnapshot) => {
         const days = [];
+        const x = [];
         querySnapshot.forEach((doc) => {
           const currentDoc = doc.data();
+
           currentDoc.id = doc.id;
           days.push(currentDoc);
+          days.map((item) => {
+            item.schedules.map((schedule) => {
+              x.push(
+                new firebase.firestore.Timestamp(
+                  schedule.seconds,
+                  schedule.nanoseconds
+                )
+              );
+            });
+          });
         });
 
         console.log(days, "dias");
+        console.log(
+          new firebase.firestore.Timestamp(
+            days[0].day.seconds,
+            days[0].day.nanoseconds
+          )
+            .toDate()
+            .toLocaleDateString(),
+          "dias"
+        );
+
+        console.log(x, "equis");
       });
   });
 
